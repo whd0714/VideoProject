ea8141243d47a5be69efa953df3110ac7c53cdee
whd07 <whd0714@naver.com>
2021-06-03 15:26:46 +0900

    Initial commit

---
+++ src/test/java/videoproject/video/VideoApplicationTests.java
@@ -0,0 +1,13 @@
+package videoproject.video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
